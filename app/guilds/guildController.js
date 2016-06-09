c720f90ff35a298f28688609d4f8eff269ca0fa4
baptiste <baptiste@bmagic.fr>
2016-06-09 21:00:09 +0200

    Add new routes to guildController

--- app/guilds/guildController.js
+++ app/guilds/guildController.js
@@ -4,13 +4,62 @@
 var applicationStorage = process.require("core/applicationStorage.js");
 var guildModel = process.require("guilds/guildModel.js");
 
-module.exports.getGuild = function (req, res, next) {
+module.exports.getGuildWithTier = function (req, res, next) {
 
     var logger = applicationStorage.logger;
     logger.info("%s %s %s %s", req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.method, req.path, JSON.stringify(req.params));
 
     var projection = {};
     projection["progress.tier_"+req.params.tier] = 1;
+
+    guildModel.find({
+        region: req.params.region,
+        realm: req.params.realm,
+        name: req.params.name
+    }, projection, function (error, guilds) {
+        if (error) {
+            logger.error(error.message);
+            res.status(500).send(error.message);
+        } else if (guilds && guilds.length > 0) {
+            res.json(guilds[0]);
+        } else {
+            next();
+        }
+    });
+};
+
+module.exports.getGuild = function (req, res, next) {
+
+    var logger = applicationStorage.logger;
+    logger.info("%s %s %s %s", req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.method, req.path, JSON.stringify(req.params));
+
+
+    guildModel.find({
+        region: req.params.region,
+        realm: req.params.realm,
+        name: req.params.name
+    }, function (error, guilds) {
+        if (error) {
+            logger.error(error.message);
+            res.status(500).send(error.message);
+        } else if (guilds && guilds.length > 0) {
+            res.json(guilds[0]);
+        } else {
+            next();
+        }
+    });
+};
+
+
+module.exports.getProgress = function (req, res, next) {
+
+    var logger = applicationStorage.logger;
+    logger.info("%s %s %s %s", req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.method, req.path, JSON.stringify(req.params));
+
+    var projection = {};
+    projection["progress.tier_"+req.params.tier+".normalCount"] = 1;
+    projection["progress.tier_"+req.params.tier+".heroicCount"] = 1;
+    projection["progress.tier_"+req.params.tier+".mythicCount"] = 1;
 
     guildModel.find({
         region: req.params.region,
--- app/guilds/routes.js
+++ app/guilds/routes.js
@@ -5,7 +5,8 @@ var router = require("express").Router();
 var guildController = process.require("guilds/guildController.js");
 
 //Define routes
-router.get('/guilds/:tier/:region/:realm/:name', guildController.getGuild);
-
+router.get('/guilds/:tier/:region/:realm/:name', guildController.getGuildWithTier);
+router.get('/guilds/:region/:realm/:name', guildController.getGuild);
+router.get('/guilds/progress/:tier/:region/:realm/:name', guildController.getProgress);
 
 module.exports = router;
