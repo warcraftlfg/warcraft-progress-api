8d75ac1597c74c75f4fe9fee11a5be4f725cd76d
baptiste <baptiste@bmagic.fr>
2016-08-24 23:32:28 +0200

    Fix bug on guildupdate priority 3 and 0 who can slow redis.

--- app/process/GuildUpdateProcess.js
+++ app/process/GuildUpdateProcess.js
@@ -44,7 +44,20 @@ GuildUpdateProcess.prototype.updateGuild = function () {
             });
         },
         function (guildUpdate, callback) {
-            if (guildUpdate.priority <= 3) {
+            if (guildUpdate.priority == 0) {
+                updateModel.getCount("wp_cu", 0, function (error, count) {
+                    if (count > 10000) {
+                        logger.info("Too many characters in priority 0 ... waiting 1 min ");
+                        updateModel.insert("wp_gu", guildUpdate.region, guildUpdate.realm, guildUpdate.name, guildUpdate.priority, function () {
+                            setTimeout(function () {
+                                callback(true);
+                            }, 60000);
+                        });
+                    } else {
+                        callback(error, guildUpdate);
+                    }
+                });
+            } else if (guildUpdate.priority == 3) {
                 updateModel.getCount("wp_cu", 3, function (error, count) {
                     if (count > 10000) {
                         logger.info("Too many characters in priority 3 ... waiting 1 min ");
