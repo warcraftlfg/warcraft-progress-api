ad97addd6ea6114792a6d20a5ffb59690804caab
baptiste <baptiste@bmagic.fr>
2016-09-20 20:09:40 +0200

    Fix bug on ranking

--- app/ranks/rankController.js
+++ app/ranks/rankController.js
@@ -126,7 +126,7 @@ module.exports.getRanking = function (req, res, next) {
 
     async.waterfall([
         function (callback) {
-            var key = req.params.tier+"#"+req.params.raid;
+            var key = "tier_"+req.params.tier+"#"+req.params.raid;
             if (req.params.realm && req.params.region) {
                 realmModel.findOne({
                     region: req.params.region,
@@ -233,7 +233,7 @@ module.exports.getRanking = function (req, res, next) {
                 }
             });
         } else {
-            res.json([]);
+            res.json({});
 
         }
     });
