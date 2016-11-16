90b4caf4a6497bde5902903675d2a7ad3475c663
Bmagic <bmagic@users.noreply.github.com>
2016-11-16 10:05:17 +0100

    Update ProgressUpdateProcess.js

--- app/process/ProgressUpdateProcess.js
+++ app/process/ProgressUpdateProcess.js
@@ -85,7 +85,7 @@ ProgressUpdateProcess.prototype.updateGuildProgress = function () {
                                                     }
                                                 }
                                             } else {
-                                                if (currentKill.count + nextKill.count >= 2) {
+                                                if (currentKill.count + nextKill.count >= 8) {
                                                     progress[difficulty][boss]["timestamps"].push([currentKill.timestamp, nextKill.timestamp]);
                                                 } else {
                                                     progress[difficulty][boss]["irrelevantTimestamps"].push([currentKill.timestamp, nextKill.timestamp]);
@@ -99,7 +99,7 @@ ProgressUpdateProcess.prototype.updateGuildProgress = function () {
 
                                     //One timestamp kill
                                     if (difficulty == "mythic") {
-                                        if (currentKill.count >= 8) {
+                                        if (currentKill.count >= 2) {
                                             if (currentKill.source == "news") {
                                                 mythicNewsProgressTimestamps.push([currentKill.timestamp]);
                                             } else {
