dd37a6d69656ce30bb11139bff9a711e2f2526f0
baptiste <baptiste@bmagic.fr>
2016-11-27 21:09:05 +0100

    Add filter for dungeon

--- app/mythicDungeons/mythicDungeonController.js
+++ app/mythicDungeons/mythicDungeonController.js
@@ -9,8 +9,24 @@ module.exports.getRanking = function (req, res, next) {
     var logger = applicationStorage.logger;
     logger.info("%s %s %s %s", req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.method, req.path, JSON.stringify(req.params));
 
-    mythicDungeonModel.find(req.params.extension, {}, function (error, runs) {
-        console.log(runs);
+    var criteria = {};
+
+    if (req.query && req.query.region) {
+        criteria.region = req.query.region;
+    }
+
+    if (req.query && req.query.affixes) {
+        criteria.affixes = req.query.affixes.split(' ');
+    }
+
+    if (req.query && req.query.dungeon) {
+
+        criteria.dungeon = req.query.dungeon;
+    }
+
+    mythicDungeonModel.find(req.params.extension, criteria, function (error, runs) {
+
+
         if (error) {
             logger.error(error.message);
             res.status(500).send(error.message);
