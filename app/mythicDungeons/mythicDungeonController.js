b089a08fecdb6aa2b389252634884db85182be0b
baptiste <baptiste@bmagic.fr>
2016-11-28 08:05:59 +0100

    Add filter for Realm & Locale

--- app/mythicDungeons/mythicDungeonController.js
+++ app/mythicDungeons/mythicDungeonController.js
@@ -1,13 +1,15 @@
 "use strict";
 
 //Load dependencies
+var async = require("async");
 var applicationStorage = process.require("core/applicationStorage.js");
 var mythicDungeonModel = process.require("mythicDungeons/mythicDungeonModel.js");
+var realmModel = process.require("realms/realmModel.js");
 
 module.exports.getRanking = function (req, res, next) {
 
     var logger = applicationStorage.logger;
-    logger.info("%s %s %s %s", req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.method, req.path, JSON.stringify(req.params));
+    logger.info("%s %s %s %s %s", req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.method, req.path, JSON.stringify(req.params), JSON.stringify(req.query));
 
     var criteria = {};
 
@@ -16,12 +18,13 @@ module.exports.getRanking = function (req, res, next) {
     }
 
     if (req.query && req.query.affixes) {
-        criteria.affixes = req.query.affixes.split(' ');
+        criteria.affixes = req.query.affixes.split('#');
     }
 
     if (req.query && req.query.dungeon) {
         criteria.dungeon = req.query.dungeon;
     }
+
     var limit;
     if (req.query && req.query.limit) {
         limit = parseInt(req.query.limit, 10);
@@ -38,16 +41,70 @@ module.exports.getRanking = function (req, res, next) {
         limit = 50;
     }
 
-    mythicDungeonModel.find(req.params.extension, criteria, limit, function (error, runs) {
 
+    async.waterfall([
+            function (callback) {
+                if (req.query && req.query.region && req.query.locale) {
+                    var localeArray = req.query.locale.split('#');
+                    if (localeArray.length == 2) {
+                        realmModel.find({
+                            "bnet.locale": localeArray[0],
+                            "bnet.timezone": {$regex: "^" + localeArray[1], $options: "i"}
+                        }, {name: 1}, function (error, realms) {
+                            if (realms.length > 0) {
+                                criteria["$or"] = [];
+                                realms.forEach(function (realm) {
+                                    criteria['$or'].push({realms: realm.name});
+                                });
+                                callback();
+                            } else {
+                                callback();
+                            }
+                        });
+                    } else {
+                        callback();
+                    }
+                } else {
+                    callback();
+                }
+            },
+            function (callback) {
+                if (req.query && req.query.region && req.query.realm) {
+                    realmModel.findOne({
+                        region: req.query.region,
+                        name: req.query.realm
+                    }, {connected_realms: 1}, function (error, realm) {
+                        if (realm) {
+                            criteria["$or"] = [];
+                            realm.connected_realms.forEach(function (realmName) {
+                                criteria['$or'].push({realms: realmName});
+                            });
+                            callback();
 
-        if (error) {
-            logger.error(error.message);
-            res.status(500).send(error.message);
-        } else if (runs && runs.length > 0) {
-            res.json(runs);
-        } else {
-            next();
+                        } else {
+                            callback();
+                        }
+                    });
+                } else {
+                    callback();
+                }
+            },
+            function (callback) {
+                mythicDungeonModel.find(req.params.extension, criteria, limit, function (error, runs) {
+                    callback(error, runs);
+                });
+            }
+
+        ],
+        function (error, runs) {
+            if (error) {
+                logger.error(error.message);
+                res.status(500).send(error.message);
+            } else if (runs && runs.length > 0) {
+                res.json(runs);
+            } else {
+                next();
+            }
         }
-    });
+    );
 };
